fa02dcd1cac7fb39f11ae11ca5352dd9593ede0c
Yunhao Mao <yunhaom94@outlook.com>
2020-07-19 00:40:21 -0400

    no decrement

--- src/Operations/GCounter.cs
+++ src/Operations/GCounter.cs
@@ -47,18 +47,6 @@ namespace RAC.Operations
 
         }
 
-        public Response Decrement()
-        {
-            Response res = new Response();
-
-
-            this.payload.valueVector[this.payload.replicaid] -= this.parameters.GetParam<int>(0);
-
-            return res;
-
-        }
-
-
     }
 
 
