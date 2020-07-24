2a1e44f45c6a11fdfd5e94a0b85c2ed465a398dc
Yunhao Mao <yunhaom94@outlook.com>
2020-07-23 22:01:26 -0400

    async handler

--- Program.cs
+++ Program.cs
@@ -2,32 +2,99 @@
 using System.IO;
 using RAC.Payloads;
 using RAC.Operations;
+using RAC.Network;
 
 using System.Collections.Generic;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Threading.Tasks.Dataflow;
+using System.Threading.Tasks;
+using System.Threading;
 
 namespace RAC
 {
 
-    class test
+    public class Producer
     {
-        public int i {get; private set;}
-        public test(int i)
+
+        // Demonstrates the production end of the producer and consumer pattern.
+        public static void Produce(ITargetBlock<string> target)
         {
-            this.i = i;
-            
+            // Create a Random object to generate random data.
+            Random rand = new Random();
+
+            // In a loop, fill a buffer with random data and
+            // post the buffer to the target block.
+            for (int i = 0; i < 100; i++)
+            {
+                // Create an array to hold random byte data.
+                string str = "string " + i;
+                Console.WriteLine("input: " + str);
+
+                // Post the result to the message block.
+                target.Post(str);
+            }
+
+            // Set the target to the completed state to signal to the consumer
+            // that no more data will be available.
         }
 
-        public override string ToString()
+        // Demonstrates the consumption end of the producer and consumer pattern.
+        public static async Task ConsumeAsync(ISourceBlock<string> source)
         {
-            return "hahaha" + i;
+            // Initialize a counter to track the number of bytes that are processed.
+            int bytesProcessed = 0;
+            string data = null;
+
+            // Read from the source buffer until the source buffer has no
+            // available output data.
+
+                while (await source.OutputAvailableAsync())
+                {
+                    data = source.Receive();
+                    Console.WriteLine(data);
+                }
+
+
         }
+
     }
+
 
     class Program
     {
+
+        private const int portNum = 13;
+
         static void Main(string[] args)
         {
-            
+            Server ss = new Server();
+
+            var handler = ss.HandleRequestAync();
+
+            ss.Run();
+
+            handler.Wait();
+
+            //ss.Run();
+
+            return;
+
+            MessagePacket mp = new MessagePacket("127", "192", "a\nb\nc");
+
+            string s = "a\nb\nc\nd";
+            StringReader reader = new StringReader(s);
+            reader.ReadLine();
+            Console.WriteLine(reader.ReadToEnd());
+
+
+
+            Console.WriteLine(new MessagePacket(mp.Serialize()));
+
+
 
             Config.numReplicas = 2;
             Config.replicaId = 0;
@@ -35,27 +102,27 @@ namespace RAC
             API.initAPIs();
 
 
-            string cmd1 = 
+            string cmd1 =
 @"gc
 0
 s
 1
 ";
 
-            string cmd2 = 
+            string cmd2 =
 @"gc
 0
 g
 ";
 
-            string cmd3 = 
+            string cmd3 =
 @"gc
 0
 i
 5
 ";
 
-            string cmd4 = 
+            string cmd4 =
 @"gc
 0
 y
@@ -66,21 +133,22 @@ y
 
             Parser.RunCommand(cmd1);
 
-            Response res  =  Parser.RunCommand(cmd2);
+            Response res = Parser.RunCommand(cmd2);
             //Console.WriteLine(res.content);
 
-            res  =  Parser.RunCommand(cmd3);
+            res = Parser.RunCommand(cmd3);
             //Console.WriteLine(res);
 
-            res  =  Parser.RunCommand(cmd4);
+            res = Parser.RunCommand(cmd4);
             //Console.WriteLine(res);
 
-            res  =  Parser.RunCommand(cmd2);
+            res = Parser.RunCommand(cmd2);
             Console.WriteLine(res);
 
-            return; 
+            return;
 
             Console.WriteLine("Hello World!");
         }
+
     }
 }
--- src/Network/Network.cs
+++ src/Network/Network.cs
@@ -1,8 +1,10 @@
 using System;
 using System.IO;
 using System.Net;
-using System.Runtime.Serialization;
 using System.Text;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+using System.Threading.Tasks.Dataflow;
 
 namespace RAC.Network
 {
@@ -21,27 +23,27 @@ namespace RAC.Network
             string s = Encoding.Unicode.GetString(bytes);
             s = s.Substring(s.IndexOf(identifier, 0, s.Length, StringComparison.Ordinal));
 
-            using (StringReader reader = new StringReader(s)) 
-            { 
+            using (StringReader reader = new StringReader(s))
+            {
                 string line;
                 int lineNumeber = 0;
                 int cl = 0;
 
-                while ((line = reader.ReadLine()) != null) 
-                { 
-                    switch(lineNumeber)
+                while ((line = reader.ReadLine()) != null)
+                {
+                    switch (lineNumeber)
                     {
-                        case 0: 
+                        case 0:
                             // header identifier
                             break;
                         case 1:
                             this.from = line + "\n";
                             break;
                         case 2:
-                            this.to = line + "\n";;
+                            this.to = line + "\n"; ;
                             break;
                         case 3:
-                            this.length = line + "\n";;
+                            this.length = line + "\n"; ;
                             cl = int.Parse(length);
                             break;
                         case 4:
@@ -51,23 +53,23 @@ namespace RAC.Network
                                 this.content = rest;
                             }
                             else if (rest.Length < cl)
-                            {   
-                                Console.WriteLine(rest.Length);    
+                            {
+                                Console.WriteLine(rest.Length);
                                 //TODO: throw error
                                 Console.WriteLine("wrong msg length");
                                 return;
-                            } 
+                            }
                             else
                             {
                                 this.content = rest.Substring(0, cl);
                             }
 
                             break;
-                        
+
 
                     }
                     lineNumeber++;
-                } 
+                }
             }
         }
 
@@ -103,6 +105,104 @@ namespace RAC.Network
         public bool isSelf;
 
 
+    }
+
+
+    public class Server
+    {
+
+        public BufferBlock<byte[]> reqQueue = new BufferBlock<byte[]>();
+
+        private IPAddress address;
+        private int port;
+
+        public Server(IPAddress address, int port)
+        {
+            this.address = address;
+            this.port = port;
+        }
+
+        public Server()
+        {
+
+        }
+
+        public void AddToQueue(byte[] bytes)
+        {
+            reqQueue.Post(bytes);
+        }
+
+        public async Task HandleRequestAync()
+        {
+            byte[] data;
+
+            while (await reqQueue.OutputAvailableAsync())
+            {
+                data = reqQueue.Receive();
+                
+                Console.WriteLine("handling ayncshoursly " + data.GetHashCode());
+            }
+
+        }
+
+        public void Run()
+        {
+            TcpListener server = null;
+            try
+            {
+                // Set the TcpListener on port 13000.
+                Int32 port = 13;
+                IPAddress localAddr = IPAddress.Parse("127.0.0.1");
+
+                // TcpListener server = new TcpListener(port);
+                server = new TcpListener(localAddr, port);
+
+                // Start listening for client requests.
+                server.Start();
+
+                // Buffer for reading data
+                Byte[] bytes = new Byte[20];
+
+                // Enter the listening loop.
+                while (true)
+                {
+                    Console.Write("Waiting for a connection... ");
+
+                    // Perform a blocking call to accept requests.
+                    // You could also use server.AcceptSocket() here.
+                    TcpClient client = server.AcceptTcpClient();
+                    Console.WriteLine("Connected!");
+
+                    // Get a stream object for reading and writing
+                    NetworkStream stream = client.GetStream();
+
+                    int i;
+
+
+                    // Loop to receive all the data sent by the client.
+                    while ((i = stream.Read(bytes, 0, bytes.Length)) != 0)
+                    {
+                        // Translate data bytes to a ASCII string.
+                        Console.WriteLine("adding new data");
+                        AddToQueue(bytes);
+                    }
+
+                    // Shutdown and end connection
+                    client.Close();
+                    
+                }
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine("SocketException: {0}", e);
+            }
+            finally
+            {
+                // Stop listening for new clients.
+                Console.WriteLine("Stopped listening");
+                server.Stop();
+            }
+        }
     }
 
 }
