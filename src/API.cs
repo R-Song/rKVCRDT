f9a1ae40d5880b51b528dc7c2749d6f062ebe758
Yunhao Mao <yunhaom94@outlook.com>
2020-07-25 18:05:04 -0400

    API file

--- src/API.cs
+++ src/API.cs
@@ -107,64 +107,6 @@ namespace RAC
             //TODO: check if all types has get, set, sync, delete after finish loading API
         }
 
-        private static void APIs() // TODO: move this to another file
-        {
-            // int
-            AddConverter("int", Converters.StringToInt, Converters.IntToString);
-            // string
-            AddConverter("string", Converters.StringToStringO, Converters.StringOToString);
-            // list of integers
-            AddConverter("listi", Converters.StringToListi, Converters.ListiToString);
-
-
-            // GCounter
-            AddNewType("GCounter", "gc");
-            AddNewAPI("GCounter", "GetValue", "g", "");
-            AddNewAPI("GCounter", "SetValue", "s", "int");
-            AddNewAPI("GCounter", "Increment", "i", "int");
-            AddNewAPI("GCounter", "Synchronization", "y", "listi");
-            
-            
-
-        }
-
-        public static class Converters
-        {
-            public static string ListiToString(object l)
-            {   
-                List<int> lst = (List<int>)l;
-                return string.Join(",", lst);
-            }
-
-            public static List<int> StringToListi(string s)
-            {   
-                return s.Split(",").Select(int.Parse).ToList();
-            }
-
-            public static string IntToString(object i)
-            {
-                return ((int)i).ToString();
-            }
-
-            public static object StringToInt(string s)
-            {
-                return int.Parse(s);
-            }
-
-            public static string StringOToString(object i)
-            {
-                return (string)i;
-            }
-
-            public static object StringToStringO(string i)
-            {
-                return (object)i;
-            }
-
-
-
-        }
-
 
 
     }
