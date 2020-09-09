1405959fd261d530fdcc363504c02625fa592437
Yunhao Mao <yunhaom94@outlook.com>
2020-09-09 18:07:23 -0400

    update some docs

--- RAC/src/Operations/Template.cs
+++ RAC/src/Operations/Template.cs
@@ -4,6 +4,18 @@ using static RAC.Errors.Log;
 
 namespace RAC.Operations
 {
+    /// <summary>
+    /// Useful things:
+    /// Payload: this.payload
+    /// Parameters: this.parameters
+    ///     get a parameter: this.parameters.GetParam<T>(int index)
+    ///     create new Parameters for broadcast: Parameters syncPm = new Parameters(int numparams);
+    ///     add values to Paramters: syncPm.AddParam(int index, object value)
+    ///     Create parameter string: Parser.BuildCommand(string typeCode, string apiCode, string uid, Parameters pm)
+    /// Create new Response: Responses res = new Responses(Status status)
+    ///     Add content to Response res.AddResponse(Dest dest, string content = "", bool includeStatus = true)
+    /// Access op history: this.history
+    /// </summary>
     public class Template : Operation<TemplatePayload>
     {
 
