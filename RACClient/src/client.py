f69b0eb7dc48985c62c69b3198c91c56066c2f6c
Gavin Gan <gan.gaving@gmail.com>
2021-03-16 20:21:49 -0700

    Add '--help' and '-h' arg, reorg folder structure

--- RACClient/src/client.py
+++ RACClient/src/client.py
@@ -257,115 +257,135 @@ class Performance:
         return res
 
 
+def isHelp(args):
+    return len(sys.argv) == 2 and (args[1] == '--help' or args[1] == '-h')
+
+def helpMessage():
+    string = ("\n  [TEMPORARY] \n\n" +
+        "  Open another terminal and run: \n\n" +
+        "  python -m http.server <port number> --bind 127.0.0.1 \n\n" +
+        "  [For Example] python -m http.server 8080 --bind 127.0.0.1 \n\n" +
+        "  and in this folder, run: \n\n" +
+        "  python client.py 127.0.0.1:<port number> \n\n" + 
+        "  [For Example] python client.py 127.0.0.1:<port number> \n")
+    print(string)
 
 if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        raise ValueError('wrong arg')
+    if isHelp(sys.argv):
+        helpMessage()
+
+    else:
+        if len(sys.argv) < 2:
+            raise ValueError('wrong arg')
     
-    address = sys.argv[1]
-    host = address.split(":")[0]
-    port = int(address.split(":")[1])
+        address = sys.argv[1]
+        host = address.split(":")[0]
+        port = int(address.split(":")[1])
 
+        print(host)
+        print(port)
 
+        s = Server(host, port)   
 
-    s = Server(host, port)   
+        if s.connect() == 0:
+            print("connection failed")
+            exit(1)
 
-    if s.connect() == 0:
-        print("connection failed")
-        exit(1)
+        while (True):
+            text = input("Enter:").split(" ")
 
-    while (True):
-        text = input("Enter:").split(" ")
+            typecode = text[0]
 
-        typecode = text[0]
+            if (typecode == "x"):
+                s.disconnect()
+                exit(0)
 
-        if (typecode == "x"):
-            s.disconnect()
-            exit(0)
+            uid = text[1]
+            opcode = text[2]
 
-        uid = text[1]
-        opcode = text[2]
+            if (typecode == "gc"):
+                gc = GCounter(s)
+                if (opcode == "g"):
+                    print(gc.get(uid))
+                if (opcode == "s"):
+                    value = text[3]
+                    print(gc.set(uid, value))
+                if (opcode == "i"):
+                    value = text[3]
+                    print(gc.inc(uid, value))
 
-        if (typecode == "gc"):
-            gc = GCounter(s)
-            if (opcode == "g"):
-                print(gc.get(uid))
-            if (opcode == "s"):
-                value = text[3]
-                print(gc.set(uid, value))
-            if (opcode == "i"):
-                value = text[3]
-                print(gc.inc(uid, value))
+            elif (typecode == "rc"):
+                rc = RCounter(s)
+                if (opcode == "g"):
+                    print(rc.get(uid))
+                if (opcode == "s"):
+                    value = text[3]
+                    print(rc.set(uid, value))
+                if (opcode == "i"):
+                    value = text[3]
+                    try:
+                        rid = text[4]
+                    except:
+                        rid = ""
+                    print(rc.inc(uid, value, rid))
+                if (opcode == "d"):
+                    value = text[5]
+                    try:
+                        rid = text[4]
+                    except:
+                        rid = ""
+                    print(rc.dec(uid, value, rid))
+                if (opcode == "r"):
+                    value = text[3]
+                    print(rc.rev(uid, value))
+                    
+            elif (typecode == "os"):
+                os = ORSet(s)
+                if (opcode == "g"):
+                    print(os.get(uid))
+                if (opcode == "s"):
+                    print(os.set(uid))
+                if (opcode == "a"):
+                    value = text[3]
+                    print(os.add(uid, value))
+                if (opcode == "rm"):
+                    value = text[3]
+                    print(os.remvoe(uid, value))
 
-        elif (typecode == "rc"):
-            rc = RCounter(s)
-            if (opcode == "g"):
-                print(rc.get(uid))
-            if (opcode == "s"):
-                value = text[3]
-                print(rc.set(uid, value))
-            if (opcode == "i"):
-                value = text[3]
-                try:
-                    rid = text[4]
-                except:
-                    rid = ""
-                print(rc.inc(uid, value, rid))
-            if (opcode == "d"):
-                value = text[5]
-                try:
-                    rid = text[4]
-                except:
-                    rid = ""
-                print(rc.dec(uid, value, rid))
-            if (opcode == "r"):
-                value = text[3]
-                print(rc.rev(uid, value))
-                
-        elif (typecode == "os"):
-            os = ORSet(s)
-            if (opcode == "g"):
-                print(os.get(uid))
-            if (opcode == "s"):
-                print(os.set(uid))
-            if (opcode == "a"):
-                value = text[3]
-                print(os.add(uid, value))
-            if (opcode == "rm"):
-                value = text[3]
-                print(os.remvoe(uid, value))
+            elif (typecode == "rg"):
+                rg = RGraph(s)
+                if (opcode == "g"):
+                    print(rg.get(uid))
+                if (opcode == "s"):
+                    print(rg.set(uid))
+                if (opcode == "av"):
+                    value = text[3]
+                    print(rg.addvertex(uid, value))
+                if (opcode == "rv"):
+                    value = text[3]
+                    print(rg.remvoevertex(uid, value))
+                if (opcode == "ae"):
+                    value1 = text[3]
+                    value2 = text[4]
+                    print(rg.addedge(uid, value1, value2))
+                if (opcode == "re"):
+                    value1 = text[3]
+                    value2 = text[4]
+                    print(rg.removeedge(uid, value1, value2))
+                if (opcode == "r"):
+                    value = text[3]
+                    print(rg.reverse(uid, value))
 
-        elif (typecode == "rg"):
-            rg = RGraph(s)
-            if (opcode == "g"):
-                print(rg.get(uid))
-            if (opcode == "s"):
-                print(rg.set(uid))
-            if (opcode == "av"):
-                value = text[3]
-                print(rg.addvertex(uid, value))
-            if (opcode == "rv"):
-                value = text[3]
-                print(rg.remvoevertex(uid, value))
-            if (opcode == "ae"):
-                value1 = text[3]
-                value2 = text[4]
-                print(rg.addedge(uid, value1, value2))
-            if (opcode == "re"):
-                value1 = text[3]
-                value2 = text[4]
-                print(rg.removeedge(uid, value1, value2))
-            if (opcode == "r"):
-                value = text[3]
-                print(rg.reverse(uid, value))
-
-        elif (typecode == "pref"):
-            pf = Performance(s)
-            if (opcode == "g"):
-                print(pf.get())
+            elif (typecode == "pref"):
+                pf = Performance(s)
+                if (opcode == "g"):
+                    print(pf.get())
 
     
-    s.disconnect()
+        s.disconnect()
+
+
+    
 
 
 
