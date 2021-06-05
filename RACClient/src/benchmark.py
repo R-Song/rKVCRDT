85ead3793cd67bafc1c75fc82c9459d1815c246f
Yunhao Mao <yunhaom94@outlook.com>
2021-06-05 19:26:53 -0400

    changed things again

--- RACClient/src/benchmark.py
+++ RACClient/src/benchmark.py
@@ -1,5 +1,5 @@
 import multiprocessing
-from multiprocessing import managers
+from multiprocessing import Manager, managers
 import string 
 import random 
 import math
@@ -29,6 +29,30 @@ def split_ipport(address):
 
     return res[0], int(res[1])
 
+def mix_lists(lists):
+    res = []
+
+    for i in range(len(lists[0])):
+        for l in lists:
+            res.append(l[i])
+
+    return res
+
+
+class Results():
+    def __init__(self, num_clients) -> None:
+        sharing = multiprocessing.Manager()
+        self.tp = []
+        self.latency = sharing.list()
+
+    def get_latency(self):
+        res = []
+        for l in self.latency:
+            if l != 0:
+                res.append(l / 1000000)
+
+        return res
+
 
 class ExperimentData():
     def __init__(self, num_objects, keys = []) -> None:
@@ -38,8 +62,13 @@ class ExperimentData():
     def CRDT(self, server):
         raise NotImplementedError
 
+    def generate_init_req(self):
+        raise NotImplementedError
         
-    def generate_op_values(self, num_ops, ops_ratio):
+    def generate_op_values(self, num_ops, ops_ratio, reverse=0):
+        raise NotImplementedError
+
+    def op_execute(self, crdt, req, last_res=""):
         raise NotImplementedError
 
     def _generate_keys(self):
@@ -91,7 +120,7 @@ class GCExperimentData(ExperimentData):
         return res
 
 
-    def generate_op_values(self, num_ops, ops_ratio):
+    def generate_op_values(self, num_ops, ops_ratio, reverse=0):
         res = []
         values = self._generate_values(num_ops, VAR_TYPE.INT)
         ops = self._generate_ops(num_ops, ops_ratio, ["i", "g"])
@@ -105,10 +134,10 @@ class GCExperimentData(ExperimentData):
             kidx += 1
             if (kidx == len(self.keys)):
                 kidx = 0
-
+    
         return res
 
-    def op_execute(self, crdt, req):
+    def op_execute(self, crdt, req, last_res=""):
         op = req[0]
         key = req[1]
         v = req[2]
@@ -121,33 +150,83 @@ class GCExperimentData(ExperimentData):
 
         return res
 
-class Results():
-    def __init__(self, num_clients) -> None:
-        sharing = multiprocessing.Manager()
-        self.tp = []
-        self.latency = sharing.list()
+class RCExperimentData(GCExperimentData):
+    def CRDT(self, server):
+        return RCounter(server)
 
-    def get_latency(self):
+    def generate_op_values(self, num_ops, ops_ratio, reverse=0):
+        '''
+        reverse: num of reverse each key has
+        '''
         res = []
-        for l in self.latency:
-            if l != 0:
-                res.append(l / 1000000)
+
+        for k in self.keys:
+            
+            reqs = []
+            values = self._generate_values(num_ops, VAR_TYPE.INT)
+            ops = self._generate_ops(num_ops, ops_ratio, ["i", "d", "g"])
+            assert len(ops) == len(values)
+
+            # reverse interval 
+            if reverse > 0:
+                r_interval = math.ceil(num_ops / (reverse + 1))
+
+
+            r_cnt = 0
+            for i in range(len(values)):
+                v = values[i]
+                op = ops[i]
+                reqs.append((op, k, v))
+
+
+                if (reverse > 0 and r_cnt < reverse and (i + 1)  % r_interval == 0):
+                    reqs.append(("r", k, ""))
+                    r_cnt += 1
+
+            if (reverse > 0 and r_cnt < reverse):
+                reqs.append(("r", k, ""))
+
+            res.append(reqs)
+
+        return res
+
+    def op_execute(self, crdt, req, last_res=""):
+        op = req[0]
+        key = req[1]
+        v = req[2]
+
+        if op == "g":
+            res = crdt.get(key)
+        elif op == "s":
+            res = crdt.set(key, v)
+        elif op == "i":
+            res = crdt.inc(key, v)
+        elif op == "d":
+            res = crdt.dec(key, v)
+        elif op == "r":
+            res = crdt.rev(key, last_res)
+        else:
+            raise ValueError("Incorrect input req: " + str(req))
 
         return res
 
 
-
 class TestRunner():
-    def __init__(self, nodes, multiplier, data) -> None:
+    
+    def __init__(self, nodes, multiplier, data, SharedManager) -> None:
         self.nodes = nodes
         self.num_nodes = len(nodes)
         self.num_clients = math.ceil(self.num_nodes * multiplier)
         self.data = data
         self.connections = self._connect()
-        print(self.connections)
         self.crdts = [self.data.CRDT(s) for s in self.connections]
         self.timing = False
+        self.do_reverse = False
         self.results = Results(self.num_clients)
+        self.rid = SharedManager.dict()
+        for k in self.data.keys:
+            self.rid[k] = ""
+        
 
     def _connect(self):
         res = []
@@ -182,7 +261,7 @@ class TestRunner():
         split = math.ceil(len(list_reqs) / len(self.crdts))
         works = []
         for i in range(0, len(list_reqs), split):
-            works.append(list_reqs[i:i + split])
+            works.append(mix_lists(list_reqs[i:i + split]))
 
         workers_pool = multiprocessing.Pool(self.num_clients)
         workers_pool.starmap(self.worker, zip(self.crdts, works))
@@ -191,9 +270,19 @@ class TestRunner():
 
     def worker(self, crdt, list_reqs):
         temp = []
+        last_rid = {}
         for req in list_reqs:
             start = time.time_ns() 
-            res = self.data.op_execute(crdt, req)
+            
+            if self.do_reverse and req[0] == "r":
+                try:
+                    res = self.data.op_execute(crdt, req, last_rid[req[1]])
+                    last_rid[req[1]] = res[1][0]
+                except Exception:
+                    continue
+            else:
+                res = self.data.op_execute(crdt, req)
+
             end = time.time_ns() 
             if (self.timing):
                 temp.append(end - start)
@@ -202,119 +291,61 @@ class TestRunner():
             self.results.latency.append(l)
 
 
-    def prep_ops(self, total_prep_ops, pre_ops_ratio):
-        reqs = self.data.generate_op_values(total_prep_ops, pre_ops_ratio)
+    def prep_ops(self, total_prep_ops, pre_ops_ratio, reverse=0):
+        if reverse > 0: 
+            self.do_reverse = True 
+        reqs = self.data.generate_op_values(total_prep_ops, pre_ops_ratio, reverse)
         self.split_work(reqs)
 
 
-    def benchmark(self, total_ops, ops_ratio, throughput = 0):
+    def benchmark(self, ops_per_object, ops_ratio, throughput = 0):
         '''
         throughput = limit # of ops per second per worker, if 0 then unlimited
         '''
+        self.do_reverse = False
         self.timing = True
 
-        reqs = self.data.generate_op_values(total_ops, ops_ratio)
+        reqs = self.data.generate_op_values(ops_per_object, ops_ratio)
         start = time.time()
         self.split_work(reqs)
         end = time.time()
 
-        self.results.tp = total_ops / (end - start)
+        self.results.tp = (ops_per_object * len(self.data.keys)) / (end - start)
         
         
 
         
 if __name__ == "__main__":
+    manager = multiprocessing.Manager()
     nodes = ["127.0.0.1:5000", "127.0.0.1:5001"]
 
-    total_objects = 10
-    total_ops = 100000
-    td = GCExperimentData(total_objects)
+    total_objects = 100
 
-    # #td = TestData(RCounter, total_objects, total_ops, [0.25, 0.25, 0.3, 0.2])
+    prep_ops_pre_obj = 1000
+    num_reverse = 100
+    prep_ratio = [0.5, 0.5, 0]
+
+    ops_per_object = 1000
+    op_ratio = [0.25, 0.25, 0.5]
+
+    #td = GCExperimentData(total_objects)   
+    td = RCExperimentData(total_objects)
     print(td.keys)
-    #print(td.generate_op_values(total_ops, [0.5, 0.5]))
-    
+
     print("Starting experiment...")
-    tr = TestRunner(nodes, 1, td)
+    tr = TestRunner(nodes, 1, td, manager)
 
     print("Initializing Data")
     tr.init_data()
 
     print("Preping Ops")
-    tr.prep_ops(20, [1, 0])
+    tr.prep_ops(prep_ops_pre_obj, prep_ratio, num_reverse)
 
+    print("Total ops:" + str(total_objects * ops_per_object))
     print("Measuing Throughput")
-    tr.benchmark(total_ops, [0.5, 0.5])
+    tr.benchmark(ops_per_object, op_ratio)
     
     print(tr.results.tp)
     print( sum(tr.results.get_latency()) / len(tr.results.get_latency()))
 
 
-
-# if __name__ == "__main__":
-#     # kv_pair = generate_kv_pair(1250)
-#     # host1 = "127.0.0.1"
-#     # port1 = 5000
-#     # s = Server(host1, port2)  
-#     # s.connect()
-
-#     # host2 = "127.0.0.1"
-#     # port2 = 5001
-#     # s2 = Server(host1, port2)  
-#     # s2.connect()
-
-#     # gcbench = RCounterBench(s, kv_pair)
-#     # gcbench.test_set()
-#     # gcbench.test_mixed_update_reverse_read(1000000)
-
-#     # s.disconnect()
-#     # gctest = GCounterTest({"127.0.0.1": 5000}, 10000)
-#     # gctest.init_data()
-#     # gctest.test_data()
-#     # gctest.test_mixed_update_read(0.5, 100000)
-#     # gctest.end()
-
-#     # rgtest = RGraphTest({"127.0.0.1": 5000}, 1000)
-#     # rgtest.init_data()
-#     # rgtest.test_data()
-#     # rgtest.set_reverse(800)
-#     # rgtest.test_mixed_update_read(0.5, 100000)
-#     # rgtest.end()
-#     proc = subprocess.Popen(["dotnet", "run", "-p", "D:\md\Project_RAC\RAC", "D:\md\Project_RAC\RAC\cluster_config.json"], stdout=subprocess.DEVNULL)
-#     time.sleep(3)
-#     proc2 = subprocess.Popen(["dotnet", "run", "-p", "D:\md\Project_RAC\RAC", "D:\md\Project_RAC\RAC\cluster_config.1.json"], stdout=subprocess.DEVNULL)
-#     time.sleep(3)
-
-#     try:
-
-#         kv_pair = generate_kv_pair(1250)
-#         host1 = "127.0.0.1"
-#         port1 = 5000
-#         s = Server(host1, port1)  
-#         s.connect()
-
-#         gctest = GCounterTest({"127.0.0.1": 5000}, 10000)
-#         gctest.init_data()
-#         #gctest.test_data()
-#         res = gctest.test_mixed_update_read(0.5, 100000)
-#         gctest.end()
-
-#         todraw = []
-#         for i in range(len(res)):
-#             todraw.append({"x": i, 1:res[i]})
-
-#         print([todraw])
-
-#         write_to_csv("x.csv", ["x", 1], todraw)
-#         headers, x, y = read_from_csv("x.csv")
-#         print(x)
-#         print(y)
-#         draw("test", "# of Ops", "Throughput", x, y)
-
-#         s.disconnect()
-
-#     finally:
-
-#         proc.terminate()
-#         proc2.terminate()
-#         print("end")
